e393614cd51be831ee1da0704c6ede9609d62002
Ali-Malik-crypto <108053769+Ali-Malik-crypto@users.noreply.github.com>
2022-11-11 19:54:45 +0500

    Update Government.sol

--- contracts/Government.sol
+++ contracts/Government.sol
@@ -1,60 +1,22 @@
-pragma solidity ^0.7.3;
+pragma solidity ^0.7.5;
 
-interface GovernmentInterface {
-    function addTransaction(address from, address to, uint amount) external;
-}
+contract Government {
 
-contract Ownable {
-
-    address public owner;
-
-    constructor() {
-        owner = msg.sender;
+    struct Transaction{
+    address from;
+    address to;
+    uint amount;
+    uint trxID;
     }
 
-    modifier OnlyOwner {
-        require(owner == msg.sender, "You are not sender");
-        _;
-    }
-}
-
-contract Bank is Ownable { 
-
-    GovernmentInterface governmentInstance = GovernmentInterface(0xD7ACd2a9FD159E69Bb102A1ca21C9a3e3A5F771B);
-
-    mapping(address => uint) balance;
-
-
-    function deposit() public payable returns(uint){
-        balance[msg.sender] += msg.value;                   
-        return balance[msg.sender];
+    Transaction[] transactionlog;
+    function addTransaction(address from, address to, uint amount) external {
+        Transaction memory _transaction = Transaction(from, to, amount, transactionlog.length);
+        transactionlog.push(_transaction);
     }
 
-    function withdrawal(uint amount) public OnlyOwner returns(uint){
-        require(balance[msg.sender] >= amount, "You dont have enought balance to send");
-        msg.sender.transfer(amount);
-        balance[msg.sender] -= amount;
-        return balance[msg.sender];       
-    }
-
-    function checkBalance() public view returns(uint){
-        return balance[msg.sender];
-    }
-
-    function getOwner() public view returns(address){
-        return owner;
-    }
-
-    function transfer(address payable recipient, uint amount) public {
-        require(balance[msg.sender] >= amount, "You dont have enought balance to send");
-        require(msg.sender != recipient, "You are sender and reciever both so its not possible");
-
-
-        recipient.transfer(amount);
-
-        governmentInstance.addTransaction(msg.sender, recipient, amount);
-
-        
+    function getTransaction(uint _index) public view returns(address, address, uint){
+        return (transactionlog[_index].from, transactionlog[_index].to, transactionlog[_index].amount);
     }
 
 }
